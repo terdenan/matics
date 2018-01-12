af77e47bad4f4e62b5c270c44baee697b309ee56
Denis Tereschenko <terdenan@gmail.com>
2018-01-12 23:47:32 +0300

    removed trash

--- source/routers/app.js
+++ source/routers/app.js
@@ -3,7 +3,6 @@ const router = require('koa-router')();
 router.get('/', async ctx => {
     const people = ['geddy', 'neil', 'alex'];
     await ctx.render('example', {people: people});
-    console.log(ctx);
 });
 
 module.exports = router;
