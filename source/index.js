07bc490af82ade933299f2071e74cd0f54831648
Denis Tereschenko <terdenan@gmail.com>
2018-01-31 15:34:50 +0300

    Error handling

--- source/index.js
+++ source/index.js
@@ -16,7 +16,9 @@ const mongoose = require('mongoose');
 
 const ApplicationError = require('libs/application-error');
 
-mongoose.connect(config.mongo.uri, {useMongoClient: true});
+mongoose.connect(config.mongo.uri, {useMongoClient: true}, function(err){
+    throw new ApplicationError('Connection to MongoDB is lost');
+});
 mongoose.Promise = global.Promise;
 
 render(app, {
@@ -52,6 +54,8 @@ app.use(async (ctx, next) => {
 
     await next();
 });
+
+console.log(config.mongo.uri)
 
 
 app.use(bodyParser);
