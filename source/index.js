b70bd4b0a869d2ee9321fe392d28f84453b8c8c2
Denis Tereschenko <terdenan@gmail.com>
2018-01-31 15:39:01 +0300

    wws

--- source/index.js
+++ source/index.js
@@ -17,6 +17,7 @@ const mongoose = require('mongoose');
 const ApplicationError = require('libs/application-error');
 
 mongoose.connect(config.mongo.uri, {useMongoClient: true}, function(err){
+    console.log(err);
     throw new ApplicationError('Connection to MongoDB is lost');
 });
 mongoose.Promise = global.Promise;
