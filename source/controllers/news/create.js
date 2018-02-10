2c92a7a1e27ecc025c48cf58a17ded8d7e43ee23
Denis Tereschenko <terdenan@gmail.com>
2018-02-10 22:23:01 +0300

    changing sitemap

--- source/controllers/news/create.js
+++ source/controllers/news/create.js
@@ -2,6 +2,9 @@
 
 const fs = require('fs');
 const path = require('path');
+const xml2js = require('xml2js');
+const util = require('util');
+const moment = require('moment');
 
 async function uploadFuckingFileAndReturnFuckingPath(file) {
     const filePath = path.join('uploads', Date.now() + '-' + file.name);
@@ -12,13 +15,37 @@ async function uploadFuckingFileAndReturnFuckingPath(file) {
     return filePath;
 }
 
+const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
+
+async function editSitemap(pathToSitemap, news) {
+    const parser = new xml2js.Parser();
+    const builder = new xml2js.Builder();
+
+    const sitemap = await readFile(pathToSitemap, 'utf-8');
+    const newRecord = {
+        loc: 'https://matics.kz/news/' + news.cyrillicTitle,
+        lastmod: moment().format(),
+        changefreq: 'monthly',
+        priority: '0.70'
+    };
+
+    parser.parseString(sitemap, async (err, result) => {
+        result.urlset.url.push(newRecord);
+        const newSitemap = builder.buildObject(result);
+        await writeFile(pathToSitemap, newSitemap);
+    });
+}
+
 module.exports = async (ctx) => {
     const news = ctx.request.body.fields;
     const file = ctx.request.body.files.file;
     
     news['photoUrl'] = await uploadFuckingFileAndReturnFuckingPath(file);
-
     const newNews = await ctx.newsModel.create(news);
+
+    const pathToSitemap = path.join('public', 'sitemap.xml');
+    await editSitemap(pathToSitemap, news);
 
     ctx.status = 200;
     ctx.body = newNews;
