17e26b0863c020f721da8416df2a8e66f47c587a
Denis Tereschenko <terdenan@gmail.com>
2018-02-15 22:34:04 +0300

    debud

--- source/controllers/news/get-news.js
+++ source/controllers/news/get-news.js
@@ -1,12 +1,14 @@
 'use strict';
 
+const marked = require('marked');
+
 module.exports = async (ctx) =>  {
     const title = ctx.params.title;
     const news = await ctx.newsModel.getBy({cyrillicTitle: title});
     const newsList = await ctx.newsModel.getRecent(3, { cyrillicTitle: { $ne: title } });
     
     if (news) {
-        await ctx.render('main/single-news', {news, newsList});
+        await ctx.render('main/single-news', {news, newsList, marked});
     } else {
         ctx.redirect('/404');
     }
